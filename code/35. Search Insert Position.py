c13e40670d8679916439a5a6b9200cf8224bca09
puhaoran <puhaoran@linkedsee.com>
2018-12-26 10:10:24 +0800

    add 35

---
+++ code/35. Search Insert Position.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if target <= nums[i]:
+                return i
+        return len(nums)
+
+'''
+题解:
+考察数组
+'''
