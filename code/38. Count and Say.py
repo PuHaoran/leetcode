b5a83e3474ac20547e5dd78a3d567965d28125ab
puhaoran <puhaoran@linkedsee.com>
2018-12-26 11:32:14 +0800

    add 38+

--- code/38. Count and Say.py
+++ code/38. Count and Say.py
@@ -28,3 +28,23 @@ class Solution:
 题解:
 字符串拼接
 '''
+import itertools
+
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        res = ['', '1']
+        for i in range(2, 31):
+            tmp_str = ''
+            for k, g in itertools.groupby(res[i-1]):
+                tmp_str += str(len(list(g))) + k
+            res.append(tmp_str)
+        return res[n]
+
+'''
+题解:
+使用python语法中的tricks，itertools.groupby()把迭代器中相邻的重复元素挑出来放在一起。
+'''
