c3ae36c599d475aefd3f69e81242cf9ab42ca747
puhaoran <puhaoran@linkedsee.com>
2018-12-26 22:36:03 +0800

    add ci-17

---
+++ coding-interview/17. 从尾到头打印链表.py
@@ -0,0 +1,33 @@
+'''
+输入一个链表的头结点，按照 从尾到头 的顺序返回节点的值。
+
+返回的结果用数组存储。
+
+样例
+输入：[2, 3, 5]
+返回：[5, 3, 2]
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def printListReversingly(self, head):
+        """
+        :type head: ListNode
+        :rtype: List[int]
+        """
+        l = []
+        while head != None:
+            l.append(head.val)
+            head = head.next
+
+        return l[::-1]
+
+'''
+题解:
+考察链表遍历
+'''
